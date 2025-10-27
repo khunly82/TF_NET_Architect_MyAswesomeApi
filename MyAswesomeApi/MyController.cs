0cc566875296bcec76e6b7feead3ff3b47b975b4
Khun <lykhun@gmail.com>
2025-10-27 16:38:02 +0100

    DB Connection

--- MyAswesomeApi/MyController.cs
+++ MyAswesomeApi/MyController.cs
@@ -1,15 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace MyAswesomeApi
 {
     [ApiController]
-    public class MyController: ControllerBase
+    public class MyController(
+        // injection de dépendences
+        IConfiguration configuration
+    ): ControllerBase
     {
 
         [HttpGet("api/hello")]
         public string Hello(string nom = "world")
         {
-            return $"Hello {nom} !!";
+            return $"Hello {nom} !!!!!!";
+        }
+
+        [HttpGet("api/pokemons")]
+        public List<Pokemon> GetPokemons()
+        {
+            // se connecter à la Db et récupérer tous les pokemons
+            // créer une connection SQL
+            // System.Sata.Sqlclient // ADO
+            using SqlConnection connection = new(configuration.GetConnectionString("Main"));
+
+            // Exécuter une requète SQL
+            // Dapper
+            List<Pokemon> pokemons = connection.Query<Pokemon>("SELECT * FROM Pokemons").ToList();
+
+            // renvoyer tous les pokémons
+            return pokemons;
         }
     }
 }

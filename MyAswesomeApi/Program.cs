30e2841a7a162ea79c55451399e37696c19d63c3
Khun <lykhun@gmail.com>
2025-11-07 16:31:27 +0100

    add cors

--- MyAswesomeApi/Program.cs
+++ MyAswesomeApi/Program.cs
@@ -2,7 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(b => b.AddDefaultPolicy(o =>
+{
+    o.AllowAnyOrigin();
+    //o.WithOrigins("https://google.be", "", "");
+    o.AllowAnyMethod();
+    o.AllowAnyHeader();
+}));
+
 var app = builder.Build();
+
+app.UseCors();
 
 // permet d'associer des routes (endpoints) à une fonction
 app.MapControllers();
